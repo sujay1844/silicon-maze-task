a59d98f35dd4dde56409e38f5d6abdff600f25a4
Sujay1844 <sujay1844@protonmail.com>
2023-08-12 19:03:33 +0530

    Added form validation

--- src/components/Form.js
+++ src/components/Form.js
@@ -12,9 +12,50 @@ export default function Form() {
     // give "Invalid last name" error message if first name is not valid
     // give "Invalid password" error message if password is not valid
     // give "Invalid email address" error message if email address is not valid
+    //
+    // Password should contain atleast 1 Capital letter,1 Small letter, 1 Symbol, 1 Number.
+    // First Name and Last Name should start with capital Letter
+    // Validate input of email field for valid email Id.
+
 
     const validateForm = () => {
-
+        // first name validation
+        if (FirstName === "") {
+            setErrorMessage("Invalid first name");
+            return;
+        }
+        if (FirstName[0] !== FirstName[0].toUpperCase()) {
+            setErrorMessage("Invalid first name");
+            return;
+        }
+        // last name validation
+        if (LastName === "") {
+            setErrorMessage("Invalid last name");
+            return;
+        }
+        if (LastName[0] !== LastName[0].toUpperCase()) {
+            setErrorMessage("Invalid last name");
+            return;
+        }
+        // password validation
+        if (Password === "") {
+            setErrorMessage("Invalid password");
+            return;
+        }
+        if (!Password.match(/^(?=.*[A-Z])(?=.*[a-z])(?=.*[\d])(?=.*[\W_]).{4,}$/)) {
+            setErrorMessage("Invalid password");
+            return;
+        }
+        // email validation
+        if (EmailId === "") {
+            setErrorMessage("Invalid email address");
+            return;
+        }
+        if (!EmailId.match(/^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/)) {
+            setErrorMessage("Invalid email address");
+            return;
+        }
+        setErrorMessage("");
     };
 
 
